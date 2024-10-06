0133f950ac4a6be10e2bd676330fdcea03332944
carib4ik <83468679+carib4ik@users.noreply.github.com>
2024-10-06 17:26:23 +0800

    Added savings

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -5,12 +5,14 @@ namespace UI
 {
     public class GameOverScreen : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI _bestScoreLabel;
         [SerializeField] private ScoreController _scoreController;
         [SerializeField] private TextMeshProUGUI _currentScoreLabel;
         
         private void OnEnable()
         {
             _currentScoreLabel.text = _scoreController.GetCurrentScore().ToString();
+            _bestScoreLabel.text = $"BEST {_scoreController.GetBestScore().ToString()}";
         }
     }
 }
--- Assets/Scripts/UI/ScoreController.cs
+++ Assets/Scripts/UI/ScoreController.cs
@@ -5,10 +5,19 @@ namespace UI
 {
     public class ScoreController : MonoBehaviour
     {
+        private const string BEST_SCORE = "BestScore"; //константа для сохранения и загрузки данных
+
+        [SerializeField] private AudioSource _bestScoreSound;
         [SerializeField] private TextMeshProUGUI _currentScoreLabel; //ссылка на текст
         [SerializeField] private int _scorePerSquare; //количество очков за куб
 
         private int _currentScore; //общее количество очков
+        private int _bestScore; //лучший результат очков
+        
+        private void Awake()
+        {
+            _bestScore = PlayerPrefs.GetInt(BEST_SCORE);
+        }
         
         public void AddScore()
         {
@@ -20,6 +29,19 @@ namespace UI
         public int GetCurrentScore()
         {
             return _currentScore;
+        }
+        
+        public int GetBestScore()
+        {
+            if (_currentScore > _bestScore)
+            {
+                _bestScore = _currentScore;
+                PlayerPrefs.SetInt(BEST_SCORE, _bestScore); //записываем значение по ключу
+                PlayerPrefs.Save(); //сохраняем данны
+                _bestScoreSound.Play();
+            }
+
+            return _bestScore;
         }
     }
 }
