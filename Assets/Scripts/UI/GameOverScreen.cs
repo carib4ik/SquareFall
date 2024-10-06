c6e1d37c0c769898a3d4832556091786151baa1c
carib4ik <83468679+carib4ik@users.noreply.github.com>
2024-10-06 16:08:05 +0800

    Added death animation + sound

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -6,10 +6,8 @@ namespace UI
     public class GameOverScreen : MonoBehaviour
     {
         [SerializeField] private ScoreController _scoreController;
-
         [SerializeField] private TextMeshProUGUI _currentScoreLabel;
-
-
+        
         private void OnEnable()
         {
             _currentScoreLabel.text = _scoreController.GetCurrentScore().ToString();
