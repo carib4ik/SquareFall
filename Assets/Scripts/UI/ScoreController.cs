090b45c2e73d41a47b0da5ffb12d9410375b7909
carib4ik <83468679+carib4ik@users.noreply.github.com>
2024-10-06 15:23:28 +0800

    Added score controller + sound

---
+++ Assets/Scripts/UI/ScoreController.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class ScoreController : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _currentScoreLabel; //ссылка на текст
+        [SerializeField] private int _scorePerSquare; //количество очков за куб
+
+        private int _currentScore; //общее количество очков
+        
+        public void AddScore()
+        {
+            _currentScore += _scorePerSquare;
+
+            _currentScoreLabel.text = _currentScore.ToString();
+        }
+    }
+}
